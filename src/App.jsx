9274eadda20d37b5498e0bc673ff90c99bde11a1
KaidoDragnil <141057660+KaidoDragnil@users.noreply.github.com>
2025-05-05 23:58:28 +0300

    Form styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import ControlledForm from './Components/Forms/ControlledForm';
-import UncontrolledForm from './Components/Forms/UncontrolledForm';
-import Game1 from './Components/Game1/Game1';
-import UseIdExample from './Components/UseIdExample/UseIdExample';
+import ApplyForm from './Components/Forms/ApplyForm';
 
 const App = () => {
   return (
     <>
-      {/* <div style={{ margin: '20px 30px' }}>Hello World</div>
-      <Game1 /> */}
-      {/* <UncontrolledForm register={register} /> */}
+      <ApplyForm />
     </>
   );
 };
---
+++ src/Components/Forms/ApplyForm.jsx
@@ -0,0 +1,83 @@
+import { Formik, Form, Field } from 'formik';
+import s from './Forms.module.css';
+
+const ApplyForm = () => {
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+  const initialValues = {
+    ownerName: '',
+    ownerEmail: '',
+    ownerPhone: '',
+    petName: '',
+    petAge: '',
+    petGender: 'male',
+    summary: '',
+    type: 'cat',
+    agree: false,
+  };
+  return (
+    <div className={s.formWrapper}>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <label>
+            <span>Owner name</span>
+            <Field name="ownerName" placeholder="Owner Name" />
+          </label>
+          <label>
+            <span>Owner email</span>
+            <Field name="ownerEmail" type="email" placeholder="Email" />
+          </label>
+          <label>
+            <span>Owner phone</span>
+            <Field name="ownerPhone" placeholder="Phone" />
+          </label>
+          <label>
+            <span>Pet name</span>
+            <Field name="petName" placeholder="Pet name" />
+          </label>
+          <label>
+            <span>Pet age</span>
+            <Field name="petAge" type="number" placeholder="Pet age" />
+          </label>
+          <div>
+            <label>
+              <Field name="petGender" type="radio" value="male" />
+              Male
+            </label>
+            <label>
+              <Field name="petGender" type="radio" value="female" />
+              Female
+            </label>
+          </div>
+          <label>
+            <span>Select pet type:</span>
+            <Field name="type" as="select">
+              <option value="cat">Cat</option>
+              <option value="dog">Dog</option>
+              <option value="crocodile">Crocodile</option>
+              <option value="python">Python</option>
+              <option value="racoon">Racoon</option>
+              <option value="elephant">Elephant</option>
+              <option value="rat">Rat</option>
+            </Field>
+          </label>
+          <label>
+            <span>Summary:</span>
+            <Field name="summary" as="textarea" rows={5} />
+          </label>
+          <div>
+            <label>
+              <Field name="agree" type="checkbox" /> I agree with rules!
+            </label>
+          </div>
+
+          <button type="submit">Apply</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default ApplyForm;
