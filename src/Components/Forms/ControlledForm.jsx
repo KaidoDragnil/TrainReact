3e49e330cb71c2146ae629dd2dd462fc22f91f32
KaidoDragnil <141057660+KaidoDragnil@users.noreply.github.com>
2025-05-05 20:08:49 +0300

    controlled form advanced

--- src/Components/Forms/ControlledForm.jsx
+++ src/Components/Forms/ControlledForm.jsx
@@ -5,30 +5,28 @@ const ControlledForm = () => {
   const [values, setValues] = useState({
     username: '',
     email: '',
+    age: '',
+    summary: '',
+    label: '',
+    role: 'fullstack',
+    accept: false,
   });
 
   const handleSubmit = e => {
     e.preventDefault();
+    console.log(values);
   };
-  /////////////////////////////////////////
-  // ///////DRY/////////
-  ///////////////////////////////
-  //   const handleChangeName = e => {setValues({ ...values, username: e.target.value });};
-  //     const handleChangeEmail = e => {setValues({ ...values, email: e.target.value });};
-  ///////////////////////////////////////////
   const handleChangeInput = e => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
     console.log(name, value);
-    ////////////////////////////////////////////////
-    //      switch (name) { case 'username':  return setValues({ ...values, username: value });
-    //       case 'email': return setValues({ ...values, email: value });
-    //       default:break; }
-    /////////////////////////////////////////////////
+    if (type === 'checkbox') {
+      return setValues({ ...values, [name]: checked });
+    }
     setValues({ ...values, [name]: value });
   };
   return (
     <section className={s.formWrapper}>
-      <form className={s.form}>
+      <form onSubmit={handleSubmit} className={s.form}>
         <label className={s.label}>
           <span>Username:</span>
           <input
@@ -49,8 +47,94 @@ const ControlledForm = () => {
             placeholder="Enter the email:"
           />
         </label>
-
-        <button className={s.button}>Register</button>
+        <label className={s.label}>
+          <span>Age:</span>
+          <input
+            onChange={handleChangeInput}
+            value={values.age}
+            className={s.input}
+            name="age"
+            type="number"
+            placeholder="Enter the age:"
+          />
+        </label>
+        <label className={s.label}>
+          <span>Summary:</span>
+          <textarea
+            onChange={handleChangeInput}
+            value={values.summary}
+            className={s.input}
+            name="summary"
+            placeholder="Enter the summary:"
+          />
+        </label>
+        <label className={s.label}>
+          <span>Label:</span>
+          <select
+            onChange={handleChangeInput}
+            value={values.label}
+            className={s.input}
+            name="label"
+          >
+            <option value="junior">junior</option>
+            <option value="middle">middle</option>
+            <option value="senior">senior</option>
+          </select>
+        </label>
+        <div>
+          <label>
+            <input
+              type="radio"
+              checked={values.role === 'backend'}
+              onChange={handleChangeInput}
+              name="role"
+              value="backend"
+            />
+            Backend
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={values.role === 'frontend'}
+              onChange={handleChangeInput}
+              name="role"
+              value="frontend"
+            />
+            Frontend
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={values.role === 'fullstack'}
+              onChange={handleChangeInput}
+              name="role"
+              value="fullstack"
+            />
+            Fullstack
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={values.role === 'devOps'}
+              onChange={handleChangeInput}
+              name="role"
+              value="devOps"
+            />
+            DevOps
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={values.accept}
+              name="accept"
+              onChange={handleChangeInput}
+            />
+            I want to see this webinar
+          </label>
+        </div>
+        <button disabled={!values.accept} className={s.button}>
+          Register
+        </button>
       </form>
     </section>
   );
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Forms</h2>
+      <h2>Module 3 Part 1</h2>
+    </header>
+  );
+};
+
+export default Header;
