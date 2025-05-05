5e5f5c837f6f425129da4fbbb1603fe788e6041a
KaidoDragnil <141057660+KaidoDragnil@users.noreply.github.com>
2025-05-05 20:47:30 +0300

    lesson3_1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ControlledForm from './Components/Forms/ControlledForm';
 import UncontrolledForm from './Components/Forms/UncontrolledForm';
 import Game1 from './Components/Game1/Game1';
+import UseIdExample from './Components/UseIdExample/UseIdExample';
 
 const App = () => {
   return (
@@ -8,7 +9,6 @@ const App = () => {
       {/* <div style={{ margin: '20px 30px' }}>Hello World</div>
       <Game1 /> */}
       {/* <UncontrolledForm register={register} /> */}
-      <ControlledForm />
     </>
   );
 };
---
+++ src/Components/UseIdExample/UseIdExample.jsx
@@ -0,0 +1,11 @@
+import CustomInput from './CustomInput.jsx';
+
+const UseIdExample = () => {
+  return (
+    <div>
+      <CustomInput />
+    </div>
+  );
+};
+
+export default UseIdExample;
